4215ef5eb43e5b39785817052b65ee7ae5e12349
appleEmily <emiri3128@gmail.com>
2019-06-10 12:43:59 +0900

    桁のズレが難しい

--- culculateRe/ViewController.swift
+++ culculateRe/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var result: UILabel!
+    
+    var number1:Int!
+    var number2:Int!
+    var number3:Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    @IBAction func plus(_ sender: Any) {
+    }
+    @IBAction func equal(_ sender: Any) {
+    }
+    @IBAction func multiple(_ sender: Any) {
+    }
+    @IBAction func devide(_ sender: Any) {
+    }
+    @IBAction func minus(_ sender: Any) {
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+//        number = 0
+//        result.text = String(number)
+    }
 }
 
